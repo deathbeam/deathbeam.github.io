e337c3f17fdfb393623ab714224f09276a18eb1d
Tomas Slusny <slusnucky@gmail.com>
2015-12-08 01:21:33 +0100

    Better tags

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ $('#main').particleground({
     dotColor: '#f5f5f5',
     lineColor: '#f5f5f5'
 });
+
+// Javascript to enable link to tab
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+}
+
+// Change hash for page-reload
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+})
